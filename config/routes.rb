7f978e53ad2582cb0164c91dfc4f6874e1e1a38b
lululala <et3216@gmail.com>
2016-04-16 14:40:51 +0800

    finish schedulation

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   root "welcome#index"
   resources :profiles
   resources :payments
+  resources :scheduleds
+  resources :evalutions
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
